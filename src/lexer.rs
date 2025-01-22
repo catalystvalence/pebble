723ef098ad1798ccf07bbc9b8efd806feca4e45f
catalystvalence <catalystvalence@github.com>
2025-01-22 18:06:37 +0200

    update lexer tests

--- src/lexer.rs
+++ src/lexer.rs
@@ -12,13 +12,14 @@ enum Token<'a> {
 
 #[derive(Debug, PartialEq)]
 enum Literal<'a> {
-    Identifier(&'a str),
-    Integer(i128),
-    Floating(f64),
-    Boolean(bool),
-    String(Cow<'a, str>),
-    Character(char),
-    Type(Type<'a>),
+    Identifier(&'a str),  // alpha_numeric_123456
+    Integer(i128),        // 123456
+    Floating(f64),        // 123.456
+    Boolean(bool),        // true
+    String(Cow<'a, str>), // "hello world"
+    Character(char),      // 'x'
+    Type(Type<'a>),       // void
+    Null,                 // null
 }
 
 #[derive(Debug, PartialEq)]
@@ -43,6 +44,10 @@ enum Operator {
     BitwiseNot,        // ~
     BitwiseLeftShift,  // <<
     BitwiseRightShift, // >>
+    Scope,             // ::
+    UnwrapPropagate,   // .?
+    Unwrap,            // .!
+    Dereference,       // .*
 }
 
 #[derive(Debug, PartialEq)]
@@ -51,7 +56,7 @@ enum Symbol {
     Assign,             // =
     TypeAssign,         // :
     FunctionTypeAssign, // ->
-    Scope,              // ::
+    MatchArm,           // =>
     Comma,              // ,
     LeftParenthesis,    // (
     RightParenthesis,   // )
@@ -86,12 +91,12 @@ enum Type<'a> {
     Ulong,                              // ulong
     Float,                              // float
     Double,                             // double
-    String,                             // string
+    String,                             // str
     Char,                               // char
     Bool,                               // bool
     Array(&'a Type<'a>, Option<usize>), // [type; count] OR [type]
     Optional(&'a Type<'a>),             // ?type
-    Reference(&'a Type<'a>),            // &type
+    Reference(&'a Type<'a>),            // *type
 }
 
 struct Lexer<'a> {
@@ -119,6 +124,30 @@ mod tests {
     use super::*;
 
     #[test]
+    fn fn_definitions() {
+        let lexer = Lexer::new(
+            "fn takes_int_returns_bool(n: int) -> bool {\n\
+                return false;\n\
+            }\n\
+            fn multiple_params_returns_void(a: str, b: [ulong], c: [str; 16]) -> void {}\n\
+            fn weird(a: [[?**str]]) -> ?byte { // a = array of array of optional reference to reference to str\n\
+                let deref: str = a[0][0].?.*.*;\n\
+                let test: ?byte = match deref {\n\
+                    \"hello\" => 1,\n\
+                    \"world\" => 2,\n\
+                    \"test\" => 3,\n\
+                    else => null,\n\
+                };\n\
+\n\
+                return test;\n\
+            }",
+        );
+
+        let tokens = lexer.collect::<Vec<_>>();
+        assert_eq!(tokens, &[]);
+    }
+
+    #[test]
     fn let_with_integers() {
         let lexer = Lexer::new(
             "let unix_ownership: byte = 0o644;\n\
@@ -128,8 +157,7 @@ mod tests {
                 let dead: int = -0xDEADBEEF;\n\
                 let undead: uint = 0xFEEDBEEF;\n\
                 let weird: long = -0o7777777777777;\n\
-                let normal: ulong = 12345678901234;\n\
-                ",
+                let normal: ulong = 12345678901234;",
         );
 
         let tokens = lexer.collect::<Vec<_>>();
